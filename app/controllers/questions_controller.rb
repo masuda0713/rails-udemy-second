3eecc10ca4dfcfe3e917ce6283b0c4b199cf91f3
masuda <masuda.fe713@gmail.com>
2025-01-23 13:56:29 +0000

    third-commit

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,41 @@
+class QuestionsController < ApplicationController
+  # 質問一覧表示
+  def index
+  end
+
+  # 質問詳細表示
+  def show
+  end
+
+  # 質問作成
+  def new
+    @question = Question.new # Questionモデルの新しいインスタンスを生成
+  end
+
+  # 質問登録
+  def create
+    # Questionモデルを初期化
+    @question = Question.new(question_params)
+    # QuestionモデルをDBへ保存
+    @question.save
+    # showへリダイレクト
+    redirect_to @question
+  end
+
+  # 質問編集
+  def edit
+  end
+
+  # 質問更新
+  def update
+  end
+
+  # 質問削除
+  def destroy
+  end
+
+  private
+  def question_params
+    params.require(:question).permit(:title, :name, :content)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # get 'users/index'
   get 'users', to: 'users#index'
+  resources :questions
 end
